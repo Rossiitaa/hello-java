fbd283b0329dc18a9e4a317c948e4de1edc2b6e8
Rosario Scuotto <102555011+Rossiitaa@users.noreply.github.com>
2023-01-24 16:22:57 +0100

    Add files via upload

---
+++ Main_bonus.java
@@ -0,0 +1,10 @@
+package org.lessons.java.bonus;
+
+public class Main_bonus {
+	public static void main(String[] args) {
+		String name = "Rosario";
+		String last_name = "Scuotto";
+		String age = "I'm 24 years old";
+		System.out.println(name + " " + last_name + " " + age);
+	}
+}
